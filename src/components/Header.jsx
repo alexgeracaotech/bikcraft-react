c7190a6575ebdd2af18660e87a9267dbd1329ae7
Alex Bessa <alexbessa.dev@gmail.com>
2025-11-08 08:41:18 -0300

    up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,6 @@
 
 const Header = ({ children }) => {
-
-  const style = `
-    h-20
-    px-50
-    bg-black
-    text-white
-    flex
-    justify-between
-    items-center
-  `;
-
-  return <header className={style}>{children}</header>;
+  return <header>{children}</header>;
 }
 
 export default Header;
